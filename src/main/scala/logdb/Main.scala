2bab9cd2e8ddeebd34360136a6efc14614c7b2fa
Todd W Crone <twcrone@gmail.com>
2021-07-01 20:05:43 -0400

    Reformat code

--- src/main/scala/logdb/Main.scala
+++ src/main/scala/logdb/Main.scala
@@ -1,27 +1,26 @@
 package logdb
 
-import scala.:+
 import scala.io.Source
 
 object Main extends App {
-  var db:Map[String, List[String]] = Map()
+  var db: Map[String, List[String]] = Map()
   val logfile = "newrelic_agent_log"
   val bufferedSource = Source.fromFile(logfile)
-  for(line <- bufferedSource.getLines()) {
+  for (line <- bufferedSource.getLines()) {
     val strings = line.split(" ")
-    for(s <- strings) {
-      if(s.startsWith("com.newrelic.agent.Transaction@")) {
+    for (s <- strings) {
+      if (s.startsWith("com.newrelic.agent.Transaction@")) {
         var transactionId = s.substring(31)
         var index = transactionId.indexOf("-")
-        if(index != -1) {
+        if (index != -1) {
           transactionId = transactionId.substring(0, index)
         }
         index = transactionId.indexOf(":")
-        if(index != -1) {
+        if (index != -1) {
           transactionId = transactionId.substring(0, index)
         }
         val listOption = db.get(transactionId)
-        if(listOption.isEmpty) {
+        if (listOption.isEmpty) {
           db += (transactionId -> List(line))
         }
         else {
